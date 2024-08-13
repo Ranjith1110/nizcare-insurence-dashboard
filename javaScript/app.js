6e243feb79ef9519585bf31b21ba5067d7787efc
Ranjith Natarajan <132538605+Ranjith1110@users.noreply.github.com>
2024-08-13 17:18:54 +0530

    app.js

--- javaScript/app.js
+++ javaScript/app.js
@@ -1,1 +1,14 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const sidebar = document.querySelector(".sidebar");
+    const bar = document.getElementById("bar");
+    const close = document.getElementById("close");
+
+    bar.addEventListener("click", function () {
+        sidebar.classList.add("active");
+    });
+
+    close.addEventListener("click", function () {
+        sidebar.classList.remove("active");
+    });
+});
 
